1e0d6c9410ed449730be75349e768e6fa2fe988c
DimaDzh <dzhartyds@gmail.com>
2024-03-11 13:22:10 +0200

    vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
         name="google-site-verification"
         content="D4ZbfYfHQmGJG_bt5GLI15BCa0pSGFGdtT-qR7b7ksI"
       />
+      <Analytics />
       <GoogleAnalytics measurementId="G-KGRLDV8Z5M" />
       <body className={`${inter.className} scroll-smooth `}>
         <HeaderLayout />
